c9a61f254b71edb713704d8916d4144d5f58df05
yuta <yuta@yutas-MacBook-Air.local>
2018-02-01 09:40:31 +0900

    set project numbers by 1 page

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: :show
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.page(params[:page]).per(16)
   end
 
   def new
