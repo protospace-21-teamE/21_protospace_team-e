5efd7b8e871034e8f0fb25ef0b7107aff67c9dd3
aaing <n.y1031@gmail.com>
2018-02-06 11:24:36 +0900

    edit preview js

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,5 +1,19 @@
 $(function(){
-  $("input[type='file']").change(function(){
-    console.log($("input[type='file']").attr("id"));
+  //fileを選択(valueが変わる)時に発火
+  $("input[type='file']").change(function(e){
+    //ファイルオブジェクトの作成
+    reader = new FileReader();
+    //読み込んだファイルの取得
+    file = e.target.files[0];
+    //ファイル読み込み終了時に発火
+    reader.onload = (function(file) {
+      return function(e){
+        //imgタグのsrcを書き換え
+        $("img").attr({
+          src: e.target.result
+        });
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
 });
