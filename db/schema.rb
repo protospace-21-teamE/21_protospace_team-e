37e0689bb2f838fc446d225e79bf29b6cf925c9a
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-06 11:24:35 +0900

    delete_unknown_comments_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405022520) do
+ActiveRecord::Schema.define(version: 20180206021826) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
