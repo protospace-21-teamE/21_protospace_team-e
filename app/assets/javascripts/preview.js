5480bcf3219d384a928f74ebbec3c82cd645c0a1
aaing <n.y1031@gmail.com>
2018-02-07 09:40:06 +0900

    edit preview.js

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -6,11 +6,16 @@ $(function(){
     //読み込んだファイルの取得
     file = e.target.files[0];
 
+    //画像以外のファイルの場合、処理を抜ける
+
+    //フォームの隣(前)にあるimagタグを取得
+    img = $(this).prev()
+
     //ファイル読み込み終了時に発火
     reader.onload = (function(file) {
       return function(e){
         //imgタグのsrcを書き換え
-        $("img").attr({
+        img.attr({
           src: e.target.result
         });
       };
