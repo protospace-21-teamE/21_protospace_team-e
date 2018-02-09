a9df57dd22507c9e7f7c690a29f333ffc81fb148
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-09 11:20:53 +0900

    set edit,destroy action to comments controller
    コメント編集、削除機能実装のためedit,destroyアクションの中身を定義。deleteアクションはまだ仮置き状態。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,12 +14,16 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy if @comment.user_id == current_user.id
+    redirect_to prototype_path(params[:prototype_id])
   end
 
   private
