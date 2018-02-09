de0d0b4ea7d7301c50434afb74f8adb572504287
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-09 13:51:05 +0900

    modify comments.js
    シングルクォートによるherokuエラー回避のため、表記を変更

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,33 +1,36 @@
-// $(function(){
-//   function buildHTML(comment){
-//     var html = `<p>
-//                   <strong>
-//                     <a href=${comment.id}>${comment.user_name}</a>
-//                     ：
-//                   </strong>
-//                   ${comment.comment}
-//                 </p>`
-//     return html;
-//   }
-//   $('#new_comment').on('submit', function(e){
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var html = buildHTML(data);
-//       $('.comments').append(html)
-//       $('.textbox').val('')
-//     })
-//     .fail(function(){
-//       alert('error');
-//     })
-//   })
-// });
+$(function(){
+  function buildHTML(comment){
+    var html =
+    '<p>' +
+      '<strong>' +
+        '<a href="">' +
+        comment.user_name +
+        '</a>' +
+        ' : ' +
+      '</strong>' +
+      comment.user_name +
+    '</p>'
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html)
+      $('.textbox').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
