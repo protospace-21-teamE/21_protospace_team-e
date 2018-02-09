7283d80dfbb0a7f0b1eac8706ba1c988aa332eb6
aaing <n.y1031@gmail.com>
2018-02-09 11:11:40 +0900

    add flash mesage

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,14 @@ class LikesController < ApplicationController
 
   def create
     Like.create(like_params)
+    flash[:notice] = 'Liked this prototype'
     redirect_to prototype_path(id: params[:prototype_id])
     # render controller: :prototype, action: :show
   end
 
   def destroy
     Like.destroy(params[:id])
+    flash[:notice] = 'Unliked this prototype'
     redirect_to prototype_path(id: params[:prototype_id])
   end
 
