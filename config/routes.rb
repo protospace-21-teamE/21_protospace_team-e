75d805dcdcba0b0c9d09ea51d6a4eff86dab9beb
aaing <n.y1031@gmail.com>
2018-02-09 11:07:03 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
 
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :like, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
