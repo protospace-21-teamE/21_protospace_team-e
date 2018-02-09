ce86b11bdecc0fd94252951e206f16bab5cc8143
aaing <n.y1031@gmail.com>
2018-02-09 11:07:39 +0900

    edit create and destroy function and set like params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,20 @@
 class LikesController < ApplicationController
 
   def create
+    Like.create(like_params)
+    redirect_to prototype_path(id: params[:prototype_id])
+    # render controller: :prototype, action: :show
   end
 
   def destroy
+    Like.destroy(params[:id])
+    redirect_to prototype_path(id: params[:prototype_id])
   end
 
   private
 
   def like_params
+    params.permit(:prototype_id).merge(user_id: current_user.id)
   end
 
 end
