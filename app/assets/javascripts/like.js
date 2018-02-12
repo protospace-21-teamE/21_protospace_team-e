6dd46e89edac4311f0bf140ad5366ebf994e3198
aaing <n.y1031@gmail.com>
2018-02-12 11:24:37 +0900

    create like.js

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,7 +1,31 @@
 $(function(){
   $("svg").on("click", function(e){
-    icon = $(this).find("path")
-    console.log(icon);
-    icon.attr("fill", "red")
+    e.preventDefault();
+    // var prototype_id = 
+    var url = location.href + "/likes"
+    var icon_svg = $(this)
+    // var formData = new FormData(this);
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      // data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      icon = icon_svg.find("path")
+      icon.attr("fill", "red")
+    })
+    .fail(function(){
+      alert('error');
+    })
+
   });
 });
+
+
+
+
