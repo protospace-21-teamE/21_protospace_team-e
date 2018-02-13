28a515aeeee034961eeb7465bea675466386a88f
aaing <n.y1031@gmail.com>
2018-02-13 09:42:33 +0900

    add comment and remove console log

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,12 +1,18 @@
 $(function(){
+  //likeボタンを押した場合
   $("#like_button").on("click", function(e){
+    //デフォルトイベント、ラップによるイベントの2重発生の回避
     e.preventDefault();
     event.stopPropagation();
+
+    //url,target, id, csrfトークンの取得
     var url = location.href + "/likes"
     var icon_svg = $(this)
     var user_id = $('.current_user_id').val();
     var prototype_id = $('.prototype_id').val();
     var csrf_token = $('meta[name=csrf-token]').attr('content');
+
+    //ajax通信を行う
     $.ajax({
       url: url,
       type: "POST",
@@ -16,16 +22,24 @@ $(function(){
       contentType: false
     })
 
+    //通信後処理
     .done(function(data){
-      console.log(data);
+      //ハートを赤に変える
       icon = icon_svg.find("path")
       icon.attr("fill", "red")
+
+      //カウントを1つ増やす
+      var likes_count = Number($("#likes_count").text())+1
+      $("#likes_count").text(likes_count)
     })
+
+    //通信失敗した場合エラーメッセージを出す
     .fail(function(){
       alert('error');
     })
   });
 
+  //unlikeボタンを押した場合,ほぼlikeと同様
   $("#unlike_button").on("click", function(e){
     e.preventDefault();
     event.stopPropagation();
@@ -44,9 +58,11 @@ $(function(){
     })
 
     .done(function(data){
-      console.log(data);
       icon = icon_svg.find("path")
       icon.attr("fill", "#D7D7D7")
+      console.log($("#likes_count").text());
+      var likes_count = Number($("#likes_count").text())-1
+      $("#likes_count").text(likes_count)
     })
     .fail(function(){
       alert('error');
