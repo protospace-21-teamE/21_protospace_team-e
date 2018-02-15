250775f91e09c6875f9f814d71b824ff9d79df2e
aaing <n.y1031@gmail.com>
2018-02-15 11:12:42 +0900

    add dependent destroy condition and validation for tag

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,7 +5,7 @@ class Prototype < ActiveRecord::Base
   has_many :users, through: :likes
   has_many :likes
   has_many :tags, through: :tag_prototypes
-  has_many :tag_prototypes
+  has_many :tag_prototypes, dependent: :destroy
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
   accepts_nested_attributes_for :tags
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   has_many :prototypes, through: :tag_prototypes
-  has_many :tag_prototypes
+  has_many :tag_prototypes, dependent: :destroy
+  validates :name, presence: true
 end
