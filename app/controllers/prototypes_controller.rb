b5c8376849ce754d819eb03bed88db04f8030a67
aaing <n.y1031@gmail.com>
2018-02-15 09:58:07 +0900

    set instance variable in prototype new action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,6 +14,7 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
     @prototype.captured_images.build
+    @prototype.tags.build
   end
 
   def create
