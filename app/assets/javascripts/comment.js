0a4607b8ecaa3637695f6007051bee8559d9769b
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-16 10:53:23 +0900

    Set delete_comment function with ajax

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -31,5 +31,26 @@ $(function(){
       alert('error');
     })
   })
+  $('.delete_comment').on('click', function(e){
+    e.preventDefault();
+    event.stopPropagation();
+    console.log($(this));
+    comment_id = $(this).data("comment_id")
+    var url = location.href + "/comments/" + comment_id
+    var csrf_token = $('meta[name=csrf-token]').attr('content');
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      headers: {"X-CSRF-Token": csrf_token},
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data.id)
+      var data_id = "#" + data.id + ""
+      $(data_id).parent().remove()
+    })
+  })
 });
 
